36c50072a67d4c88e3e10ac9eba8ae819499dc07
Thenameisshyam <shyam5454@gmail.com>
2024-08-31 22:03:00 +0530

    Initial commit

---
+++ handson/src/main/java/com/ms_hands_on/handson/BooksMsApplication.java
@@ -0,0 +1,13 @@
+package com.ms_hands_on.handson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMsApplication.class, args);
+	}
+
+}
---
+++ handson/src/test/java/com/ms_hands_on/handson/BooksMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_hands_on.handson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
